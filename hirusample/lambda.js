3431fafb4eb41f575b29fc55240e2efaabb781bc
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-05 15:54:46 +0530

    recog add

--- hirusample/lambda.js
+++ hirusample/lambda.js
@@ -1,4 +1,27 @@
+const AWS = require('aws-sdk');
+const ddb = new AWS.DynamoDB.DocumentClient();
+const rekog = new AWS.Rekognition();
+
 exports.handler = function(event, context, callback) {
-    console.log("success")
-    callback(null, {"message": "Successfully executed"});
+    let s3 = event.Records[0].s3;
+    rekog.detectLabels({
+        Image: {
+            S3Object: {
+                Bucket: s3.bucket.name,
+                Name: s3.object.key
+            }
+        },
+        MaxLabels: 1
+    }).promise()
+        .then(data => {
+            console.log(data);
+            if (!data.Labels || data.Labels.length < 1) {
+                callback(null, {});
+                return;
+            }
+            let lbl = data.Labels[0];
+            console.log(lbl.Name)
+
+        })
+        .catch(callback);
 }
